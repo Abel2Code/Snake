2d7f559a4b819af1f31aa0fbcd74fea9ec480f71
Abel2Code <asalinaswc1@aol.com>
2017-04-21 18:29:11 -0700

    Added character movement

--- BoardGUIPane.java
+++ BoardGUIPane.java
@@ -15,19 +15,75 @@ public class BoardGUIPane extends GridPane{
 	}
 	
 	public void moveUp(){
-		
+		int tempRow = snake.getRow();
+		int tempCol = snake.getCol();
+		if(snake.getRow() == 0){
+			snake.setRow(rows - 1);
+			labels[rows - 1][tempCol].getStyleClass().clear();
+			labels[rows - 1][tempCol].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		} else{
+			snake.setRow(tempRow - 1);
+			labels[tempRow - 1][tempCol].getStyleClass().clear();
+			labels[tempRow - 1][tempCol].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		}
 	}
 	
 	public void moveDown(){
-		
+		int tempRow = snake.getRow();
+		int tempCol = snake.getCol();
+		if(snake.getRow() == rows - 1){
+			snake.setRow(0);
+			labels[0][tempCol].getStyleClass().clear();
+			labels[0][tempCol].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		} else{
+			snake.setRow(tempRow + 1);
+			labels[tempRow + 1][tempCol].getStyleClass().clear();
+			labels[tempRow + 1][tempCol].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		}
 	}
 	
 	public void moveLeft(){
-		
+		int tempRow = snake.getRow();
+		int tempCol = snake.getCol();
+		if(snake.getCol() == 0){
+			snake.setCol(columns - 1);
+			labels[tempRow][columns - 1].getStyleClass().clear();
+			labels[tempRow][columns - 1].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		} else{
+			snake.setCol(tempCol - 1);
+			labels[tempRow][tempCol- 1].getStyleClass().clear();
+			labels[tempRow][tempCol - 1].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		}
 	}
 	
 	public void moveRight(){
-		
+		int tempRow = snake.getRow();
+		int tempCol = snake.getCol();
+		if(snake.getCol() == columns - 1){
+			snake.setCol(0);
+			labels[tempRow][0].getStyleClass().clear();
+			labels[tempRow][0].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		} else{
+			snake.setCol(tempCol + 1);
+			labels[tempRow][tempCol + 1].getStyleClass().clear();
+			labels[tempRow][tempCol + 1].getStyleClass().add("snake");
+			labels[tempRow][tempCol].getStyleClass().clear();
+			labels[tempRow][tempCol].getStyleClass().add("board");
+		}
 	}
 	
 
